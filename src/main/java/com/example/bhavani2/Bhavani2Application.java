50119d0313eedcc38e5d0dec6d9719ac174f3ae9
unknown <y.gangabhavani123@gmail.com>
2019-06-22 10:39:58 +0530

    first commit

---
+++ src/main/java/com/example/bhavani2/Bhavani2Application.java
@@ -0,0 +1,13 @@
+package com.example.bhavani2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bhavani2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bhavani2Application.class, args);
+	}
+
+}
